8827b2c735d5468a8e07f46c73479b82b923affd
Abhilash <abhilashmeher1234@gmail.com>
2022-04-04 12:02:22 +0530

    Added program file for UC_1

---
+++ MoodAnalyzerProblem/MoodAnalyze.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyze
+    {
+        
+        public string AnalyzeMood(string message)
+        {
+
+            if (message.Contains("SAD"))//Check string
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+
+        }
+    }
+
+}
---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MoodAnalyzerProblem;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Arrange
+            string expexted = "SAD";
+            string message = "I am in SAD Mood";
+            MoodAnalyze moodA = new MoodAnalyze();
+
+            //Act
+            string mood = moodA.AnalyzeMood(message);
+
+
+            //Assert
+            Assert.AreEqual(expexted, mood);
+        }
+
+    }
+}
